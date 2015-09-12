dd89f0ead9dcd8876fdb6ce7091a6efcaa9b6ef4
Christopher Reed <careed.lubbock@outlook.com>
2015-09-12 09:26:48 -0500

    Updating files....

--- ExamPrepDemo/MainPage.xaml.cs
+++ ExamPrepDemo/MainPage.xaml.cs
@@ -17,14 +17,61 @@ using Windows.UI.Xaml.Navigation;
 
 namespace ExamPrepDemo
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+		}
+
+		private void TogglePaneButton_Checked(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void BackButton_Click(object sender, RoutedEventArgs e)
+		{
+			bool ignored = false;
+
+			this.BackRequested(ref ignored);
+		}
+
+		private void BackRequested(ref bool handled)
+		{
+			if (this.frame == null)
+			{
+				return;
+			}
+
+			if (this.frame.CanGoBack && !handled)
+			{
+				handled = true;
+
+				this.frame.GoBack();
+			}
+		}
+
+		private void OnNavigatingToPage(object sender, NavigatingCancelEventArgs e)
+		{
+		}
+
+		private void OnNavigatedToPage(object sender, NavigationEventArgs e)
+		{
+			if (e.Content is Page && e.Content != null)
+			{
+				var control = (Page)e.Content;
+
+				control.Loaded += Page_Loaded;
+			}
+		}
+
+		private void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+			((Page)sender).Focus(FocusState.Programmatic);
+			((Page)sender).Loaded -= Page_Loaded;
+		}
+	}
 }
